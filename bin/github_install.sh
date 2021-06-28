498d8fd47c068eec0219c5a1605c0a5eede4ec90
Stalker-Return <eduard.babyshenko@google.com>
2021-06-29 00:01:01 +0200

    Dayly upload

--- bin/github_install.sh
+++ bin/github_install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 /usr/bin/git --git-dir=$HOME/.git/install/ --work-tree=$HOME/Clouds/Dropbox/BackUps/Linux/install/ add .
-/usr/bin/git --git-dir=$HOME/.git/install/ --work-tree=$HOME/Clouds/Dropbox/BackUps/Linux/install/ commit -m "Minor Changes"
+exitcode1=$?
+/usr/bin/git --git-dir=$HOME/.git/install/ --work-tree=$HOME/Clouds/Dropbox/BackUps/Linux/install/ commit -m "Regular Update"
+exitcode2=$?
 /usr/bin/git --git-dir=$HOME/.git/install/ --work-tree=$HOME/Clouds/Dropbox/BackUps/Linux/install/ push -u
+exitcode3=$?
 /usr/bin/git --git-dir=$HOME/.git/install/ --work-tree=$HOME/Clouds/Dropbox/BackUps/Linux/install/ status
-echo "github-tools updated: $(date)" >> $HOME/.local/share/log-files/install.log
+exitcode4=$?
+echo "github-install updated: $(date), exit codes:$exitcode1:$exitcode2:$exitcode3:$exitcode4" >> $HOME/.local/share/log-files/github_install.log
